9f37877b9164a550b15e26e2fb7659d0473da88e
OLeDouxEt <samtemartin6@outlook.com>
2023-06-27 00:43:18 -0500

    Just added the SKU tier to the deployment script

--- StaticWebApp-Deploy.ps1
+++ StaticWebApp-Deploy.ps1
@@ -1,3 +1,3 @@
 Get-AzResourceProvider
 Register-AzResourceProvider -ProviderNamespace "Microsoft.Web"
-New-AzStaticWebApp -Name "personalsite" -ResourceGroupName "SkyLab" -Location "centralus"
+New-AzStaticWebApp -Name "personalsite" -ResourceGroupName "SkyLab" -Location "centralus" -SkuName "Free"
